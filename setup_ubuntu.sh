9f4b7a5723059a1db2e6d033295f6773b9e8d272
Szabó Krisztián <developer.krisz@gmail.com>
2024-11-07 00:22:52 +0200

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,32 +1,45 @@
-sudo apt update
+#!/bin/bash
+set -e
+
+# Update package list and upgrade installed packages
+sudo apt update && sudo apt upgrade -y
+
+# npm for neovim
+# https://github.com/ajeetdsouza/zoxide
+# https://github.com/junegunn/fzf#installation
+# https://github.com/sharkdp/fd?tab=readme-ov-file#installation
+# https://github.com/BurntSushi/ripgrep/blob/master/README.md#installation
+# https://github.com/sharkdp/bat#on-ubuntu-using-apt
+sudo apt install -y \
+                stow xclip trash-cli btop \
+                python3 python3-pip python3-dev \
+                npm zoxide fzf fd-find ripgrep bat \  
+                build-essential curl wget git \
 
 
-# essentials
-sudo apt install -y stow \
-                 xclip \
-                 trash-cli
 
 # PROGRAMMING
-# python
-sudo apt install -y python3 python3-pip python3-dev
 
-# rust
+# Install Rust and update PATH
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo 'source "$HOME/.cargo/env"' >> ~/.bashrc
+echo 'source "$HOME/.cargo/env"' >> ~/.zshrc
 # need to source cargo for the current shell
-. "$HOME/.cargo/env" 
+source "$HOME/.cargo/env"
 
-# npm
-sudo apt install npm -y  # needed for neovim
 
 # goland
-sudo apt install golang -y  # needed for yq
+# sudo apt install golang -y  # needed for yq
 
 # neovim 4 lunarvim
 # sudo apt install -y neovim  # last time this installed 0.6, too old
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-echo 'add to bashrc/zshrc:\n export PATH="$PATH:/opt/nvim-linux64/bin"'
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+# sudo rm -rf /opt/nvim
+# sudo tar -C /opt -xzf nvim-linux64.tar.gz
+# echo 'add to bashrc/zshrc:\n export PATH="$PATH:/opt/nvim-linux64/bin"'
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install -y neovim
 
 # COMMUNITY MAINTAINED
 # exa is unmaintained use fork: eza
@@ -41,64 +54,67 @@ echo 'add to bashrc/zshrc:\n export PATH="$PATH:/opt/nvim-linux64/bin"'
 # sudo apt install -y eza
 
 # https://github.com/eza-community/eza/blob/main/INSTALL.md#cargo-cratesio
-cargo install eza
+# https://github.com/Orange-OpenSource/hurl?tab=readme-ov-file#cargo
+# https://github.com/Byron/dua-cli
+# 'better' man pages / pip3 install tldr
+cargo install eza hurl dua-cli tealdeer
 
 # alternatively lsd
 # https://github.com/lsd-rs/lsd?tab=readme-ov-file#prerequisites
 # sudo apt install lsd
 
 # https://github.com/jesseduffield/lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-
-# https://github.com/ajeetdsouza/zoxide
-sudo apt install zoxide
+# LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+# tar xf lazygit.tar.gz lazygit
+# sudo install lazygit /usr/local/bin
+sudo add-apt-repository ppa:lazygit-team/release -y
+sudo apt update
+sudo apt install -y lazygit
 
 # https://github.com/mikefarah/yq
-# sudo add-apt-repository ppa:rmescandon/yq
-# sudo apt update
-# sudo apt install yq -y
-go install github.com/mikefarah/yq/v4@latest
+sudo snap install yq
 
-# https://github.com/Orange-OpenSource/hurl?tab=readme-ov-file#cargo
-cargo install hurl
-
-# https://github.com/Byron/dua-cli
-cargo install dua-cli
 
 # https://github.com/PaulJuliusMartinez/jless
 # rust toolcahin needed
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # cargo install jless  
 
-# https://github.com/junegunn/fzf#installation
-sudo apt install fzf
-# https://github.com/sharkdp/fd?tab=readme-ov-file#installation
-sudo apt install fd-find
-# https://github.com/BurntSushi/ripgrep/blob/master/README.md#installation
-sudo apt install ripgrep
-# https://github.com/sharkdp/bat#on-ubuntu-using-apt
-sudo apt install bat -y
-
 # need ripgrep, lazygit before running this
 # https://www.lunarvim.org/docs/installation#release
 yes y | LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
-# 'better' man pages
-# pip3 install tldr
-cargo install tealdeer
 
 # simple terminal ui for docker and docker compose
 go install github.com/jesseduffield/lazydocker@latest
 
-# setup oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Install Oh My Zsh if not present
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Clone Oh My Zsh plugins
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
 
 # stow --adopt -nvt ~ *
 echo "if you make changes, use: \n\n  stow --adopt -v -n -t ~ *\n\nto update your current zshrc \n(delete -n flag to actually run it)"
+
+# Clean up
+sudo apt autoremove -y
+sudo apt clean
+
 echo "setup done"
