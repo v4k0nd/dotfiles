c8322b18e9755b77c5c826e3c01c9cd5b1ac1801
Szabó Krisztián <developer.krisz@gmail.com>
2024-11-07 01:21:22 +0200

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,7 +4,6 @@ set -e
 # Update package list and upgrade installed packages
 sudo apt update && sudo apt upgrade -y
 
-# npm for neovim
 # https://github.com/ajeetdsouza/zoxide
 # https://github.com/junegunn/fzf#installation
 # https://github.com/sharkdp/fd?tab=readme-ov-file#installation
@@ -13,10 +12,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
                 stow xclip trash-cli btop \
                 python3 python3-pip python3-dev \
-                npm zoxide fzf fd-find ripgrep bat \  
-                build-essential curl wget git \
-
-
+                zoxide fzf fd-find ripgrep bat \
+                build-essential curl wget git jq
 
 # PROGRAMMING
 
@@ -30,16 +27,25 @@ source "$HOME/.cargo/env"
 
 # goland
 # sudo apt install golang -y  # needed for yq
+GO_VERSION="1.23.3"
+if [ ! -d "/usr/local/go" ]; then
+    wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+    export PATH=$PATH:/usr/local/go/bin
+    rm go${GO_VERSION}.linux-amd64.tar.gz
+fi
 
 # neovim 4 lunarvim
 # sudo apt install -y neovim  # last time this installed 0.6, too old
-# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-# sudo rm -rf /opt/nvim
-# sudo tar -C /opt -xzf nvim-linux64.tar.gz
-# echo 'add to bashrc/zshrc:\n export PATH="$PATH:/opt/nvim-linux64/bin"'
-sudo add-apt-repository ppa:neovim-ppa/stable -y
-sudo apt update
-sudo apt install -y neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo 'add to bashrc/zshrc:\n export PATH="$PATH:/opt/nvim-linux64/bin"'
+# sudo add-apt-repository ppa:neovim-ppa/stable -y
+# sudo apt update
+# sudo apt install -y neovim
 
 # COMMUNITY MAINTAINED
 # exa is unmaintained use fork: eza
@@ -64,27 +70,25 @@ cargo install eza hurl dua-cli tealdeer
 # sudo apt install lsd
 
 # https://github.com/jesseduffield/lazygit
-# LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-# curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-# tar xf lazygit.tar.gz lazygit
-# sudo install lazygit /usr/local/bin
-sudo add-apt-repository ppa:lazygit-team/release -y
-sudo apt update
-sudo apt install -y lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+# sudo add-apt-repository ppa:lazygit-team/release -y
+# sudo apt update
+# sudo apt install -y lazygit
+
 
 # https://github.com/mikefarah/yq
 sudo snap install yq
 
 
 # https://github.com/PaulJuliusMartinez/jless
-# rust toolcahin needed
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # cargo install jless  
 
 # need ripgrep, lazygit before running this
 # https://www.lunarvim.org/docs/installation#release
-yes y | LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
-
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
 # simple terminal ui for docker and docker compose
 go install github.com/jesseduffield/lazydocker@latest
